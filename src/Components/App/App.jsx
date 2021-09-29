373bd1235dd170f7a761bb4a9a8806dd1252adf4
CrioUser <crio-user@criodo.com>
2021-09-30 03:07:07 +0530

    on input headerExpanded

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -7,14 +7,21 @@ import './App.css';
 
 class App extends React.Component {
     state = {
-        headerText: "Name It!"
+        headerText: "Name It!",
+        headerExpanded: true
+    };
+
+    handleInputChange = (inputText) => {
+        this.setState({ headerExpanded: !inputText });
     };
 
     render () {
         return (
         <div>
-            <Header headtitle={this.state.headerText}/>
-            <SearchBox/>
+            <Header
+             headerExpanded={this.state.headerExpanded} 
+             headtitle={this.state.headerText}/>
+            <SearchBox onInputChange={this.handleInputChange}/>
         </div>
         );
     }
