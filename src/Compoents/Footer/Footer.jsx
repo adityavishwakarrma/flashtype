7121b228421bc664ec5fb2b1062a9885464a1128
CrioUser <crio-user@criodo.com>
2021-10-04 23:53:16 +0530

    adding the footer component

---
+++ src/Compoents/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <a
+             href="https://in.linkedin.com/in/adityavishwakarrma"
+             className="footer-link"
+             target="_blank"
+             rel="noreferrer"
+            >
+                Watch the my LinkedIn profile now!
+            </a>
+        </div>
+    );
+}
+
+export default Footer;
